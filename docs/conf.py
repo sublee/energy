a123e3ff5bab0477a3e1001d2119e67e824d72b1
Heungsub Lee <sub@subl.ee>
2013-08-20 21:08:27 +0900

    change ga tracking code

--- docs/conf.py
+++ docs/conf.py
@@ -98,7 +98,7 @@ html_theme = 'energy'
 # further.  For a list of options available for each theme, see the
 # documentation.
 html_theme_options = {'github_fork': 'sublee/energy',
-                      'google_analytics': 'UA-28655602-1'}
+                      'google_analytics': 'UA-28655602-5'}
 
 # Add any paths that contain custom themes here, relative to this directory.
 html_theme_path = ['_themes']
